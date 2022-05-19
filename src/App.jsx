d301bdef79cfa826cbbd4b2928f52f73b1ea6626
tlee0818 <texeirareborn@gmail.com>
2022-05-19 18:26:57 +0900

    general css stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/Header"
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Projects from './components/projects/Projects';
+import Contacts from './components/contacts/Contacts';
+import Footer from './components/footer/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Nav />
+      <About /> 
+      <Experience />
+      <Projects />
+      <Contacts />
+      <Footer />
+
     </div>
+
   );
 }
 
