70e37102d41b3963bb8ac348dc2bbc2d46ee0bcb
tlee0818 <texeirareborn@gmail.com>
2022-05-24 18:25:46 +0900

    form submission completed

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -2,8 +2,32 @@ import React from "react";
 import "./contacts.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsLinkedin, BsInstagram } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contacts = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ittwpgs",
+        "template_ck1euao",
+        form.current,
+        "FWjMsBNFnbYJUJhPi"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  
   return (
     <section id="contacts">
       <h5>always looking for exciting opportunities</h5>
@@ -11,7 +35,6 @@ const Contacts = () => {
 
       <div className="container contact-container">
         <div className="contact-options">
-
           <a href="mailto:takhol@andrew.cmu.edu" target="_blank">
             <article className="contact-option">
               <MdOutlineEmail className="contact-option-icon" />
@@ -39,8 +62,7 @@ const Contacts = () => {
           </a>
         </div>
 
-
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
           <textarea
