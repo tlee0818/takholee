1dffe1d12b5d56c53a3b65b6d2c701060258a090
tlee0818 <texeirareborn@gmail.com>
2022-05-25 15:52:31 +0900

    made footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,22 @@ import "./footer.css"
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      {/* <a href='#' className='footer-logo'>TAKHOLEE</a> */}
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#projects'>Projects</a></li>
+        <li><a href='#contacts'>Contacts</a></li>
+      </ul>
+
+
+      <div className="footer-copyright">
+        <small>&copy; Tak-Ho Lee 2022. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
